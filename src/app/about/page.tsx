cc19b70dc2f72795bb8c148e3debd6a83a112775
Dhruv Roy <dhruvakr9001@gmail.com>
2025-01-20 20:00:20 +0530

    dept responsive

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -23,17 +23,17 @@ const page = () => {
         <h1 className="text-7xl font-bold z-10 px-5 mt-20 text-center ">
           Our Departments
         </h1>
-        <div className="z-50 mt-20 w-full flex flex-col items-center gap-10 ">
+        <div className="z-50 mt-20 w-full flex flex-col items-center gap-10 mb-10 ">
           {Departments.map((dept) => (
-            <div key={dept.name} className="w-2/3">
+            <div key={dept.name} className="w-3/4 duration-300 lg:w-full bg-gray-900">
               <DropdownCard title={dept.name}>
-                <p className="text-lg font-medium">{dept.description}</p>
+                <p className="text-xl font-medium my-3 italic">Role : {dept.description}</p>
                 <div className="flex flex-col w-full justify-between items-center">
                   <figure className="w-full">
-                    <figcaption className="text-3xl font-semibold my-5">
+                    <figcaption className="text-3xl font-semibold my-5 text-yellow-400">
                       Heads
                     </figcaption>
-                    <ul className="flex items-center justify-center gap-4">
+                    <ul className={`flex items-center justify-center gap-5  ${dept.Heads.length > 2 ? "flex-col lg:flex-row" : "flex-col md:flex-row"} `}>
                       {dept.Heads?.map((head) => (
                         <li
                           key={head}
@@ -52,28 +52,28 @@ const page = () => {
                     </ul>
                   </figure>
 
-                  <div className="w-full flex justify-between items-center">
+                  <div className="w-full flex flex-col md:flex-row justify-between items-start gap-y-5 text-center">
                     <figure className="w-full">
-                      <figcaption className="text-3xl font-semibold my-5">
+                      <figcaption className="text-3xl font-semibold my-5 text-blue-500">
                         Executives
                       </figcaption>
                       <ul className="flex flex-col gap-1">
                         {dept.Executives?.map((Exc) => (
-                          <li key={Exc} className="mx-4 flex ">
-                            <p className="text-2xl font-medium">{Exc}</p>
+                          <li key={Exc} className="mx-4 flex">
+                            <p className="text-2xl w-full font-medium text-center ">{Exc}</p>
                           </li>
                         ))}
                       </ul>
                     </figure>
 
                     <figure className="w-full">
-                      <figcaption className="text-3xl font-semibold my-5">
+                      <figcaption className="text-3xl font-semibold my-5 text-pink-500">
                         Volunteers
                       </figcaption>
                       <ul className="flex flex-col gap-1">
                         {dept.Volunteers?.map((Vol) => (
                           <li key={Vol} className="mx-4 flex ">
-                            <p className="text-2xl font-medium">{Vol}</p>
+                            <p className="text-2xl font-medium w-full text-center ">{Vol}</p>
                           </li>
                         ))}
                       </ul>
