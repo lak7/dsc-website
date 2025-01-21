e62a78e95fff69c5514b1fe694f47427cb340ece
saparya05 <saparya05@gmail.com>
2025-01-21 14:02:09 +0530

    Footer Links Updated

--- src/components/common/SocialMedia.tsx
+++ src/components/common/SocialMedia.tsx
@@ -3,11 +3,11 @@ import { FaLinkedin, FaInstagram } from "react-icons/fa";
 const Linkedin = () => {
   return (
     <a
-      href=  "https://instagram.com"
+      href= "https://in.linkedin.com"
       target="_blank"
       rel="noopener noreferrer"
-      className="text-4xl text-blue-600 hover:text-blue-800"
-    >
+      className="text-4xl text-pink-500 hover:text-pink-700"
+    >    
       <FaLinkedin />
     </a>
   );
@@ -16,10 +16,10 @@ const Linkedin = () => {
 const Instagram = () => {
   return (
     <a
-      href= "linkin.com"
+      href=  "https://instagram.com"
       target="_blank"
       rel="noopener noreferrer"
-      className="text-4xl text-pink-500 hover:text-pink-700"
+      className="text-4xl text-blue-600 hover:text-blue-800"
     >
       <FaInstagram />
     </a>
